54cf1999de38b661ecc97884c2b4f093b1774f84
aewering <andreas.ewering@andrena.de>
2023-12-17 19:38:53 +0100

    add new encoders/decoders for protoc-gen-elm json api

--- src/Protobuf/Utils/Timestamp.elm
+++ src/Protobuf/Utils/Timestamp.elm
@@ -1,11 +1,14 @@
-module Protobuf.Utils.Timestamp exposing (posixToTimestamp, timestampToPosix)
+module Protobuf.Utils.Timestamp exposing (posixToTimestamp, timestampToPosix, timestampJsonEncoder, timestampJsonDecoder)
 
 {-| Conversions between the Protobuf Well-Known Type "Timestamp" and Elm's `Time.Posix`.
 
-@docs posixToTimestamp, timestampToPosix
+@docs posixToTimestamp, timestampToPosix, timestampJsonEncoder, timestampJsonDecoder
 
 -}
 
+import Iso8601
+import Json.Decode
+import Json.Encode
 import Protobuf.Types.Int64 as Int64 exposing (Int64)
 import Time
 
@@ -47,3 +50,28 @@ timestampToPosix { seconds, nanos } =
             lowerSeconds * 1000 + nanos // 1000000
     in
     Time.millisToPosix millis
+
+
+{-| Custom JSON encoder for timestamps as ISO-8601 date strings
+-}
+timestampJsonEncoder : Timestamp -> Json.Encode.Value
+timestampJsonEncoder timestamp =
+    timestampToPosix timestamp
+        |> Iso8601.fromTime
+        |> Json.Encode.string
+
+
+{-| Custom JSON decoder for timestamps as ISO-8601 date strings
+-}
+timestampJsonDecoder : Json.Decode.Decoder Timestamp
+timestampJsonDecoder =
+    Json.Decode.string
+        |> Json.Decode.andThen
+            (\str ->
+                case Iso8601.toTime str of
+                    Ok posix ->
+                        Json.Decode.succeed (posixToTimestamp posix)
+
+                    Err _ ->
+                        Json.Decode.fail <| "Expected Iso8601 string, received: " ++ str
+            )
