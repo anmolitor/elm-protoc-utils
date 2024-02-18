b0071c1d1bd96f1b7189577d1abc55f8e0f87f8e
aewering <andreas.ewering@andrena.de>
2024-02-18 18:26:43 +0100

    release new version for int64 string parsers

--- src/Protobuf/Utils/Int64.elm
+++ src/Protobuf/Utils/Int64.elm
@@ -1,8 +1,12 @@
-module Protobuf.Utils.Int64 exposing (toSignedString, toUnsignedString)
+module Protobuf.Utils.Int64 exposing
+    ( toSignedString, toUnsignedString
+    ,fromSignedString, fromUnsignedString
+    )
 
 {-| Utility methods for Int64 needed in the Protobuf/gRPC context.
 
 @docs toSignedString, toUnsignedString
+@docs fromSignedString, fromUnsignedString
 
 -}
 
@@ -63,7 +67,105 @@ toUnsignedString int64 =
         ( upper, lower ) =
             Int64.toInts int64
     in
-    toUnsignedStringHelp upper lower ""
+    toUnsignedStringHelp (Bitwise.shiftRightZfBy 0 upper) (Bitwise.shiftRightZfBy 0 lower) ""
+
+
+{-| Parse a signed Int64 from its string representation
+
+    fromSignedString "10"
+        --> Just (Int64 { higher = 0, lower = 10 })
+
+    fromSignedString "-10"
+        --> Just (Int64 { higher = -1, lower = -10 })
+
+    fromSignedString "not-a-number"
+        --> Nothing
+
+    fromSignedString "9223372036854775807"
+        --> Just (Int64 { higher = 2147483647, lower = -1 })
+
+    fromSignedString "-9223372036854775808"
+        --> Just (Int64 { higher = -2147483648, lower = 0 })
+
+    fromSignedString "-9223372036854775809"
+        --> Nothing
+
+-}
+fromSignedString : String -> Maybe Int64
+fromSignedString str =
+    case String.uncons str of
+        Just ( '-', rest ) ->
+            if (String.length rest == 19 && rest > "9223372036854775808") || String.length rest > 19 then
+                Nothing
+
+            else
+                Maybe.map
+                    (\int64 ->
+                        let
+                            ( newH, newL ) =
+                                complement int64 |> add 1
+                        in
+                        Int64.fromInts newH newL
+                    )
+                    (fromUnsignedStringHelp rest ( 0, 0 ))
+
+        _ ->
+            if (String.length str == 19 && str > "9223372036854775807") || String.length str > 19 then
+                Nothing
+
+            else
+                fromUnsignedStringHelp str ( 0, 0 )
+                    |> Maybe.map (\( x, y ) -> Int64.fromInts x y)
+
+
+{-| Parse an unsigned Int64 from its string representation
+
+    fromUnsignedString "10"
+        --> Just (Int64 { higher = 0, lower = 10 })
+
+    fromUnsignedString "-10"
+        --> Nothing
+
+    fromUnsignedString "not-a-number"
+        --> Nothing
+
+    fromUnsignedString "9223372036854775807"
+        --> Just (Int64 { higher = 2147483647, lower = -1 })
+
+    fromUnsignedString "18446744073709551615"
+        --> Just (Int64 { higher = -1, lower = -1 })
+
+    fromUnsignedString "18446744073709551616"
+        --> Nothing
+
+-}
+fromUnsignedString : String -> Maybe Int64
+fromUnsignedString str =
+    if (String.length str == 20 && str > "18446744073709551615") || String.startsWith "-" str || String.length str > 20 then
+        Nothing
+
+    else
+        fromUnsignedStringHelp str ( 0, 0 ) |> Maybe.map (\( x, y ) -> Int64.fromInts x y)
+
+
+maxSafeDigitsAtOnce : Int
+maxSafeDigitsAtOnce =
+    6
+
+
+fromUnsignedStringHelp : String -> ( Int, Int ) -> Maybe ( Int, Int )
+fromUnsignedStringHelp str int64 =
+    case ( String.slice 0 maxSafeDigitsAtOnce str |> String.toInt, String.dropLeft maxSafeDigitsAtOnce str ) of
+        ( Nothing, _ ) ->
+            Nothing
+
+        ( Just int, "" ) ->
+            multiply (10 ^ String.length str) int64 |> add int |> Just
+
+        ( Just int, rest ) ->
+            multiply (10 ^ maxSafeDigitsAtOnce) int64
+                |> add int
+                |> fromUnsignedStringHelp rest
 
 
 toUnsignedStringHelp : Int -> Int -> String -> String
@@ -90,3 +192,34 @@ complement ( a, b ) =
     ( Bitwise.complement a |> Bitwise.shiftRightZfBy 0
     , Bitwise.complement b |> Bitwise.shiftRightZfBy 0
     )
+
+
+multiply : Int -> ( Int, Int ) -> ( Int, Int )
+multiply n ( higher, lower ) =
+    let
+        lowerFull =
+            lower * n
+
+        lowerCarry =
+            lowerFull // 0x0000000100000000
+
+        newLower =
+            lowerFull - lowerCarry * 0x0000000100000000
+
+        newHigher =
+            higher * n + lowerCarry
+    in
+    ( newHigher, newLower )
+
+
+add : Int -> ( Int, Int ) -> ( Int, Int )
+add n ( higher, lower ) =
+    let
+        newLower =
+            lower + n
+    in
+    if newLower > 0xFFFFFFFF then
+        ( higher + 1, Bitwise.and 0xFFFFFFFF newLower )
+
+    else
+        ( higher, newLower )
